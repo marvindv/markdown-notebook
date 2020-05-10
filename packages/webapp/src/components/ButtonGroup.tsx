75f9c1e312aa0e93ec7f413eeb66737f733783a7
Marvin Davieds <marvin.davieds@gmail.com>
2020-05-10 19:13:00 +0200

    feat(webapp): add ButtonGroup component

---
+++ packages/webapp/src/components/ButtonGroup.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Button from './Button';
+
+const ButtonGroup = styled.div`
+  display: flex;
+
+  ${Button} {
+    flex: 1;
+  }
+
+  ${Button}:not(:last-child) {
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
+  }
+
+  ${Button}:not(:first-child) {
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
+  }
+`;
+
+export default ButtonGroup;
