fed3d0ed637ca0869a174c20877a820f0229cd98
Marvin Davieds <marvin.davieds@gmail.com>
2020-05-11 13:54:13 +0200

    feat(webapp): reset current path on new login

--- packages/webapp/src/features/navigation/currentPathSlice.ts
+++ packages/webapp/src/features/navigation/currentPathSlice.ts
@@ -1,11 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { changeCurrentApi } from 'src/features/api/apiSlice';
 import { changeEntityTitle } from 'src/features/notebooks/notebooksSlice';
 import Path from 'src/models/path';
 
-const PATH_KEY = '_markdown_notebook_current_path';
+const PATH_LOCAL_STORAGE_KEY = '_markdown_notebook_current_path';
 
+/**
+ * Loads the current path from the localStorage or returns null if there is none
+ * saved.
+ *
+ * @returns {(Path | null)}
+ */
 function fromLocalStorage(): Path | null {
-  const state = localStorage.getItem(PATH_KEY);
+  const state = localStorage.getItem(PATH_LOCAL_STORAGE_KEY);
   if (!state) {
     return null;
   }
@@ -13,8 +20,14 @@ function fromLocalStorage(): Path | null {
   return JSON.parse(state);
 }
 
+/**
+ * Saves the given path into the localStorage to be loaded with
+ * {@link fromLocalStorage}.
+ *
+ * @param {Path} path
+ */
 function intoLocalStorage(path: Path) {
-  localStorage.setItem(PATH_KEY, JSON.stringify(path));
+  localStorage.setItem(PATH_LOCAL_STORAGE_KEY, JSON.stringify(path));
 }
 
 const currentPathSlice = createSlice({
@@ -27,6 +40,13 @@ const currentPathSlice = createSlice({
     },
   },
   extraReducers: builder => {
+    // Whenever the api changes, i.e. on a new login, reset the current path.
+    builder.addCase(changeCurrentApi, () => {
+      intoLocalStorage({});
+      return {};
+    });
+
+    // Update the current path if the title of one of the components changed.
     builder.addCase(changeEntityTitle.fulfilled, (state, { payload }) => {
       const { oldPath, newTitle } = payload;
 
