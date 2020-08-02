725c8ca4ac601a6934dc48fbe2105cd8f23ea453
Marvin Davieds <marvin.davieds@gmail.com>
2020-08-02 11:57:00 +0200

    test(webapp): remove App test

--- packages/webapp/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import App from './App';
-
-test('renders the App component', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeTruthy();
-});
