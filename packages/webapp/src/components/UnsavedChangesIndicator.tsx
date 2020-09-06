772e69c60d6e7badff6d52f3de0b4273c2c197b4
Marvin Davieds <marvin.davieds@gmail.com>
2020-09-06 20:24:33 +0200

    feat(webapp): use theme color in UnsavedChangesIndicator

--- packages/webapp/src/components/UnsavedChangesIndicator.tsx
+++ packages/webapp/src/components/UnsavedChangesIndicator.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: -0.2em 0;
-  color: #333;
+  color: ${props => props.theme.baseColors.foreground};
 
   svg {
     font-size: 0.5em;
