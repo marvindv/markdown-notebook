60045cd6f13df1679b5220f019be509e9ae0b6e4
Marvin Davieds <marvin.davieds@gmail.com>
2020-11-08 02:19:10 +0100

    feat(webapp): save expanded nodes state in localstorage

--- packages/webapp/src/features/navigation/expandedNodesSlice.ts
+++ packages/webapp/src/features/navigation/expandedNodesSlice.ts
@@ -9,11 +9,24 @@ import {
 } from 'src/models/tree';
 import { ExpandedNodesTree } from './FileTree';
 
-const initialState: ExpandedNodesTree = createEmptyTree<true>();
+const EXPANDED_NODES_STORAGE_KEY = '_markdown_notebook_expanded_nodes';
+
+function fromLocalStorage(): ExpandedNodesTree | null {
+  const state = localStorage.getItem(EXPANDED_NODES_STORAGE_KEY);
+  if (!state) {
+    return null;
+  }
+
+  return JSON.parse(state);
+}
+
+function intoLocalStorage(tree: ExpandedNodesTree) {
+  localStorage.setItem(EXPANDED_NODES_STORAGE_KEY, JSON.stringify(tree));
+}
 
 const expandedNodesSlice = createSlice({
   name: 'expandedNodes',
-  initialState,
+  initialState: fromLocalStorage() || createEmptyTree<true>(),
   reducers: {
     /**
      * Sets the expanded state for the given node.
@@ -26,6 +39,7 @@ const expandedNodesSlice = createSlice({
       const payload = isExpanded ? true : undefined;
 
       setTreeNodePayload(state, path, payload);
+      intoLocalStorage(state);
     },
 
     /**
@@ -41,16 +55,21 @@ const expandedNodesSlice = createSlice({
       for (let i = 0; i < path.length; i++) {
         setTreeNodePayload(state, path.slice(0, i + 1), payload);
       }
+
+      intoLocalStorage(state);
     },
   },
   extraReducers: builder => {
     // Reset whenever the api is changed.
     builder.addCase(changeCurrentApi, () => {
-      return createEmptyTree();
+      const state: ExpandedNodesTree = createEmptyTree();
+      intoLocalStorage(state);
+      return state;
     });
 
     builder.addCase(changeNodeName.fulfilled, (state, { payload }) => {
       changeTreeNodeName(state, payload.oldPath, payload.newName);
+      intoLocalStorage(state);
     });
   },
 });
