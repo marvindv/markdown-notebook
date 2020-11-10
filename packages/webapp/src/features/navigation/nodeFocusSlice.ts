cdfbf714535fe316325c2fb266c17420d1a8617f
Marvin Davieds <marvin.davieds@gmail.com>
2020-11-10 22:27:59 +0100

    fix(webapp): handle api and node name change in nodeFocusSlice

--- packages/webapp/src/features/navigation/nodeFocusSlice.ts
+++ packages/webapp/src/features/navigation/nodeFocusSlice.ts
@@ -1,6 +1,13 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { changeCurrentApi } from 'src/features/api/apiSlice';
+import { changeNodeName } from 'src/features/nodes/nodesSlice';
 import { Path } from 'src/models/node';
-import { createEmptyTree, setTreeNodePayload, Tree } from 'src/models/tree';
+import {
+  changeTreeNodeName,
+  createEmptyTree,
+  setTreeNodePayload,
+  Tree,
+} from 'src/models/tree';
 
 interface State {
   isFocusNodePendingTree: Tree<true>;
@@ -33,6 +40,28 @@ const nodeFocusSlice = createSlice({
       const { path, isHighlighted } = action.payload;
       setTreeNodePayload(state.highlightedNodeTree, path, isHighlighted);
     },
+  },
+  extraReducers: builder => {
+    // Reset whenever the api is changed.
+    builder.addCase(changeCurrentApi, () => {
+      return {
+        isFocusNodePendingTree: createEmptyTree(),
+        highlightedNodeTree: createEmptyTree(),
+      };
+    });
+
+    builder.addCase(changeNodeName.fulfilled, (state, { payload }) => {
+      changeTreeNodeName(
+        state.highlightedNodeTree,
+        payload.oldPath,
+        payload.newName
+      );
+      changeTreeNodeName(
+        state.isFocusNodePendingTree,
+        payload.oldPath,
+        payload.newName
+      );
+    });
   },
 });
 
